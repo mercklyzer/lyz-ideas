fddfe44e49519bc6b02598c09ea6bbc072c3d817
Lyzer Merck Bautista <66999325+mercklyzer@users.noreply.github.com>
2022-05-04 21:14:39 +0800

    Delete lyz-ideas-client directory

--- lyz-ideas-client/src/hooks/useUser.js
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux"
-
-export const useUser = () => {
-    const user = useSelector(state => state.user.currentUser)
-    const token = useSelector(state => state.user.token)
-    const role = useSelector(state => state.user.currentUser?.isAdmin)
-
-    return [user, token, role]
-}
